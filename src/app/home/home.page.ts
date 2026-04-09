fed981526376d77b62f75008ed72c9a47a8949a6
Lucas <lucasmirandabr0508@gmail.com>
2026-04-09 19:28:07 +0000

    Formulario em Angular

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,16 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
+  veiculo: any = {
+    modelo: null,
+    marca: null,
+    cor: null
+  }
 
-  constructor( 
-    public crudService: CrudService
-  ){ }
+  veiculos: any = []
 
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
+  cadastrar(){
+    this.veiculos.push(this.veiculo);
   }
 
 }
